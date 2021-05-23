f3d2dd8c465ad54de1b1a63db6dc83a78eb53149
Felix Isaac <limfelix16@gmail.com>
2021-05-24 01:30:31 +0800

    update: create initial data and logic after install

--- src/background.js
+++ src/background.js
@@ -1,6 +1,39 @@
-let color = '#3aa757';
+const initialOptions = Object.freeze({
+  daily_limit: 60, // in minutes
+  weekly_limit: 500, // in minutes
+  block_type: 1, // enum: FIXED: 1, RANDOM: 2, BOTH: 3
+  block_next_episode_button: true, // shows block screen before clicking 'NEXT EPISODE' button
+  block_next_episode: true, // show s block screen before an episode starts
+})
+
+const initialData = {
+  current_day: {
+    day: null,
+    minutes_spent: 0
+  },
+  days: [], // Array of objects, each object with two fields (day, minutes_spent)
+}
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  // set init data
+  chrome.storage.sync.get('days', ({ days }) => !days && chrome.storage.sync.set(initialData));
+  resetSettings();
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined) {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { method: "time" }, (response) => {
+        console.info(`Timer initialized. Response from content script: ${response || 'No response'}`);
+      });
+    });
+  }
+});
+
+function resetSettings() {
+  chrome.storage.sync.set(initialOptions);
+  console.info('Reset extension options across synced Google user data.')
+}
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,33 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() { 
-    const currentPercentage = 40;
-    const [percentageText] = document.getElementsByClassName('percentage');
-    const [percentageCircle] = document.getElementsByClassName('circle');
-    
-    percentageText.innerHTML = percentageText.innerHTML.replace('{{percentage}}', currentPercentage);
-    percentageCircle.style.strokeDasharray = `${currentPercentage}, 100`
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.storage.sync.get('daily_limit', ({ daily_limit }) => {
+        const currentPercentage = 0;
+        const [percentageText] = document.getElementsByClassName('percentage');
+        const [percentageCircle] = document.getElementsByClassName('circle');
+        
+        percentageText.innerHTML = percentageText.innerHTML.replace('{{percentage}}', currentPercentage);
+        percentageCircle.style.strokeDasharray = `${currentPercentage}, 100`
+    })
 });
-
-// // Initialize butotn with users's prefered color
-// let changeColor = document.getElementById("changeColor");
-
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
-
-// // When the button is clicked, inject setPageBackgroundColor into current page
-// changeColor.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-// });
-
-// // The body of this function will be execuetd as a content script inside the
-// // current page
-// function setPageBackgroundColor() {
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     document.body.style.backgroundColor = color;
-//   });
-// }
