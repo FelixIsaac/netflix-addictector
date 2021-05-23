2d44775066af5372242f8ad301e743d620395101
Felix Isaac <limfelix16@gmail.com>
2021-05-23 21:05:32 +0800

    update(popup): added basic logic

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,24 +1,33 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+document.addEventListener("DOMContentLoaded", function() { 
+    const currentPercentage = 40;
+    const [percentageText] = document.getElementsByClassName('percentage');
+    const [percentageCircle] = document.getElementsByClassName('circle');
+    
+    percentageText.innerHTML = percentageText.innerHTML.replace('{{percentage}}', currentPercentage);
+    percentageCircle.style.strokeDasharray = `${currentPercentage}, 100`
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// // Initialize butotn with users's prefered color
+// let changeColor = document.getElementById("changeColor");
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.backgroundColor = color;
+// });
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageBackgroundColor,
+//   });
+// });
+
+// // The body of this function will be execuetd as a content script inside the
+// // current page
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
