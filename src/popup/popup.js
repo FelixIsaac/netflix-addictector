d3a51ed7ea50634cd136ff4704ac6cf2e6f29420
Felix Isaac <limfelix16@gmail.com>
2021-06-02 13:42:03 +0800

    update(popup): add quotes design

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -6,43 +6,62 @@ document.addEventListener('DOMContentLoaded', function () {
     const monthHours = document.getElementById('month-hours');
     const installationHours = document.getElementById('installation-hours')
     const optionsButton = document.getElementById('options');
+    const quoteContent = document.getElementsByTagName('blockquote')[0];
+    const quoteAuthor = document.querySelector('section figure figcaption');
 
-    optionsButton.addEventListener('click', () => chrome.runtime.openOptionsPage())
+    optionsButton.addEventListener('click', () => chrome.runtime.openOptionsPage());
+    chrome.storage.onChanged.addListener(({ daily_limit, current_day, days }) => (daily_limit || current_day || days) && updatePopup());
+    
+    updatePopup();
+    updateQuote();
 
-    chrome.storage.sync.get(['daily_limit', 'current_day', 'days'], ({
-        daily_limit,
-        current_day,
-        days
-    }) => {
-        if (!current_day) {
-            current_day = {
-                day: Date.now(),
-                minutes_spent: 0
+    setInterval(updateQuote, 30 * 1000); // 30 second interval
+
+    function updatePopup() {
+        chrome.storage.sync.get(['daily_limit', 'current_day', 'days'], ({
+            daily_limit,
+            current_day,
+            days
+        }) => {
+            if (!current_day) {
+                current_day = {
+                    day: Date.now(),
+                    minutes_spent: 0
+                }
+
+                chrome.storage.sync.set(current_day);
             }
 
-            chrome.storage.sync.set(current_day);
-        }
+            // overview stats
+            const currentPercentage = ((current_day.minutes_spent / daily_limit) * 100).toFixed(2);
+            percentageText.innerHTML = percentageText.innerHTML.replace('{{minutes_spent}}', parseFloat(current_day.minutes_spent.toFixed(1)));
+            percentageAboutText.innerHTML = percentageAboutText.innerHTML.replace('{{time_limit}}', daily_limit);
+            percentageCircle.style.strokeDasharray = `${currentPercentage}, 100`
 
-        // overview stats
-        const currentPercentage = ((current_day.minutes_spent / daily_limit) * 100).toFixed(2);
-        percentageText.innerHTML = percentageText.innerHTML.replace('{{minutes_spent}}', parseFloat(current_day.minutes_spent.toFixed(1)));
-        percentageAboutText.innerHTML = percentageAboutText.innerHTML.replace('{{time_limit}}', daily_limit);
-        percentageCircle.style.strokeDasharray = `${currentPercentage}, 100`
-
-        // hours spent
-        weekHours.innerText = [...days.splice(days.length - 6), current_day]
-            .map(days => days.minutes_spent/60)
-            .reduce((a, b) => a + b, 0)
-            .toFixed(1);
+            // hours spent
+            weekHours.innerText = [...days.splice(days.length - 6), current_day]
+                .map(days => days.minutes_spent/60)
+                .reduce((a, b) => a + b, 0)
+                .toFixed(1);
+                
+            monthHours.innerText = [...days.splice(days.length - 29), current_day]
+                .map(days => days.minutes_spent/60)
+                .reduce((a, b) => a + b, 0)
+                .toFixed(1);
             
-        monthHours.innerText = [...days.splice(days.length - 29), current_day]
-            .map(days => days.minutes_spent/60)
-            .reduce((a, b) => a + b, 0)
-            .toFixed(1);
-        
-        installationHours.innerText = [...days, current_day]
-            .map(days => days.minutes_spent/60)
-            .reduce((a, b) => a + b, 0)
-            .toFixed(1);
-    })
+            installationHours.innerText = [...days, current_day]
+                .map(days => days.minutes_spent/60)
+                .reduce((a, b) => a + b, 0)
+                .toFixed(1);
+        });
+    };
+
+    function updateQuote() {
+        const [quote, author] = ['', ''];
+        quoteContent.innerText = quoteContent.innerText.replace('{{quote}}', quote);
+        quoteAuthor.innerText = quoteAuthor.innerText.replace('{{author}}', author);
+    }
 });
+
+// update overview if there's new day entry
+addTime(false, 0);
