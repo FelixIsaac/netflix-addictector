8604732ebc90698386f978f3d94aaeea1ad4028d
Felix Isaac <limfelix16@gmail.com>
2021-06-09 16:03:21 +0800

    update: support for safari extension

---
+++ Netflix Addictector – Netflix Screen Time/Netflix Addictector – Netflix Screen Time/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Netflix Addictector – Netflix Screen Time
+//
+//  Created by Felix Isaac on 9/6/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
